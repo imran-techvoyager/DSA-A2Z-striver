7fb1975337dfcf0e4df65a79463bf11b92884bac
imran-techvoyager <imran.techvoyager@gmail.com>
2025-01-11 21:38:18 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -2,6 +2,23 @@
 #include <iomanip>
 using namespace std;
 
+//always write in terms of functions
+//since in coding platforms there is
+//no int main() instead there are test cases
+
+void print1(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
+
 int main()
 {
 
@@ -9,10 +26,26 @@ for (int i = 0; i < 4; i++)
 {
     for (int j = 0; j < 4; j++)
     {
-        cout<<"*";
+        cout<<"* ";
     }
     cout<<endl;
 }
+
+int n;
+cout<<"enter the number of stars you want to print:"<<endl;
+cin>>n;
+print1(n);
+
+//this is test case example in cp
+// int p;
+// cin>>p;
+// for (int q = 0; q < p; q++)
+// {
+//     int r;
+//     cin>>r;
+//     print1(r);
+// }
+
 
 
 return 0;
---
+++ patterns2.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void print2(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<"* ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
+
+void print3(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<j<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+
+void print4(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<i<<" ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
+
+void print5(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        int y = (n-i)+1;
+
+        for (int j = 0; j < y; j++)
+        {
+           cout<<"* ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
+void print6(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        int y = (n-i)+1;
+
+        for (int j = 1; j <= y; j++)
+        {
+           cout<<j<<" ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
+
+void print7(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout<<" ";
+        }
+        
+        for (int j = 0; j < 2*i+1; j++)
+        {
+            cout<<"*";
+        }
+        
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout<<" ";
+        }
+
+        cout<<endl;
+    }
+
+}
+
+//this doesnt work
+// void print8(int n){
+//     for(int i = 1; i <= n; i++){
+//         for (int j = 1; j <= n; j++)
+//         {
+//             cout<<"* ";
+//             n--;
+//         }
+        
+//         cout<<endl;
+//     }
+// }
+
+void print9(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout<<" ";
+        }
+        
+        for (int j = 0; j < (2*n)-(2*i)-1; j++)
+        {
+            cout<<"*";
+        }
+        
+
+        for (int j = 0; j < i; j++)
+        {
+            cout<<" ";
+        }
+    
+        cout<<endl;
+
+    }
+}
+
+int main()
+{
+
+int n;
+cin>>n;
+print2(n);
+print3(n);
+print4(n);
+print5(n);
+print6(n);
+print7(n);
+// print8(n);
+print9(n);
+
+return 0;
+}
