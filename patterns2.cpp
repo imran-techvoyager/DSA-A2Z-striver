cdffa495412ec5ec47956608b668d7c66aafc3da
imran-techvoyager <imran.techvoyager@gmail.com>
2025-01-13 13:52:59 +0530

    new patterns

--- patterns2.cpp
+++ patterns2.cpp
@@ -148,20 +148,120 @@ void print10(int n){
     
 }
 
+void print11(int n){
+    for(int i = 0; i < n; i++){
+        int start = 1;
+        if (i%2==0)
+        {
+            start = 1;
+        }
+        else{
+            start = 0;
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<start<<" ";
+            start = 1 - start;
+        }
+        cout<<endl;
+        
+    }
+}
+
+void print12(int n){
+    for (int i = 1; i <= n; i++)
+    { 
+       //number
+      for (int j = 1; j <= i; j++)
+      {
+        cout<<j;
+      }
+      
+       //space
+       int space = 2*(n-i);
+      for (int j = 1; j <= space; j++)
+      {
+        cout<<" ";
+      }
+      
+
+       //number  
+       for (int j = i; j >= 1; j--)
+      {
+        cout<<j;
+      }
+      cout<<endl;
+    }
+    
+
+}
+
+void print13(int n){
+    int num = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+    
+}
+
+void print14(int n){
+    
+    for (int i = 0; i < n; i++)
+    {
+        char alphabet = 'A';
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<alphabet<<" ";
+            alphabet++;
+        }
+        cout<<endl;
+        
+    }
+    
+}
+void print15(int n){
+    
+    for (int i = 0; i < n; i++)
+    {
+        char alphabet = 'A';
+        for (int j = 0; j < n-i; j++)
+        {
+            cout<<alphabet<<" ";
+            alphabet++;
+        }
+        cout<<endl;
+        
+    }
+    
+}
+
+
 int main()
 {
 
 int n;
 cin>>n;
-print2(n);
-print3(n);
-print4(n);
-print5(n);
-print6(n);
-print7(n);
+// print2(n);
+// print3(n);
+// print4(n);
+// print5(n);
+// print6(n);
+// print7(n);
 // print8(n);
-print9(n);
-print10(n);
+// print9(n);
+// print10(n);
+// print11(n);
+// print12(n);
+// print13(n);
+// print14(n);
+// print15(n);
+// print16(n);
 
 return 0;
 }
