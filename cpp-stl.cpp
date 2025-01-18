01c393390cc4d8e188e38c5bfd2c8e260b3afc1b
imran-techvoyager <imran.techvoyager@gmail.com>
2025-01-18 22:52:24 +0530

    stl

---
+++ cpp-stl.cpp
@@ -0,0 +1,13 @@
+//this includes all basic or commonly used libraries 
+//this is good for competitive programing but not for project 
+//in projects we need to have all header files included which are required in the project
+#include<bits/stdc++.h>
+// #include <iostream>
+// #include <iomanip>
+using namespace std;
+
+int main()
+{
+
+return 0;
+}
